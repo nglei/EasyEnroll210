f8a3238cc56ca2e187d797a767153767369e5208
Jackshooter <wei_sakai@hotmail.my>
2019-03-10 03:52:51 +0800

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function sidemenuout(){
+    document.getElementById("clickonmenu").style.width="250px";
+    document.getElementById("main").style.marginLeft="250px";
+}
+function closemenu(){
+    document.getElementById("clickonmenu").style.width="0px";
+    document.getElementById("main").style.marginLeft="0px";
+}
