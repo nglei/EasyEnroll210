26177a53e7b2c4295602b275723a35466e47204f
Jackshooter <wei_sakai@hotmail.my>
2019-03-11 16:04:57 +0800

    final 210 commit

--- Personal Website Prj/TRUE/js/main.js
+++
@@ -1,8 +0,0 @@
-function sidemenuout(){
-    document.getElementById("clickonmenu").style.width="250px";
-    document.getElementById("main").style.marginLeft="250px";
-}
-function closemenu(){
-    document.getElementById("clickonmenu").style.width="0px";
-    document.getElementById("main").style.marginLeft="0px";
-}
