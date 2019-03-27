bf3384a9dc37255ebff41ae4dab1073108276734
Jackshooter <wei_sakai@hotmail.my>
2019-03-27 16:42:43 +0800

    Commitoncemore

--- main/js/main.js
+++ main/js/main.js
@@ -1,8 +1,10 @@
 function sidemenuout(){
+    //slide content away to fit in sidepanel menu
     document.getElementById("clickonmenu").style.width="250px";
     document.getElementById("main").style.marginLeft="250px";
 }
 function closemenu(){
+    //close the menu the content is slided back
     document.getElementById("clickonmenu").style.width="0px";
     document.getElementById("main").style.marginLeft="0px";
 }
